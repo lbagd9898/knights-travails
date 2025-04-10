52763c2903d1d0efb9d9ffde3aac85eb12aa1331
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-04-09 20:31:53 -0400

    final push for knights travails project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # knights-travails
+
+Knights Travails Project from Odin Project.
+
+This project helped me to learn about graphs and Breadth-First Search in graphs.
--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Node class represents each position on the chess board and the possible knight moves each position can make
 class Node {
   constructor(data) {
     this.data = data;
@@ -5,11 +6,13 @@ class Node {
   }
 }
 
+//Graph class to represent the board and link the nodes according to movement of teh knight
 class Graph {
   constructor() {
-    this.nodes = {};
+    this.nodes = {}; //stores nodes with keys format [x,y]
   }
 
+  //Construct a 7 x 7 chessboard with unique node at every position
   constructGraph() {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
@@ -19,6 +22,7 @@ class Graph {
     }
   }
 
+  //adds to nodes each Node's adjacentNodes parameter, linking them according to which other nodes a knight can move to in one turn
   linkNodes() {
     for (let key in this.nodes) {
       let [x, y] = JSON.parse(key);
@@ -89,28 +93,34 @@ class Graph {
     }
   }
 
+  //ptints all nodes and their keys
   printNodes() {
     for (let key in this.nodes) {
       console.log(`Key: ${key}, Value: ${this.nodes[key]}`);
     }
   }
 
+  //prints individual node including it's data and neighbors
   printNode(key) {
     let node = this.nodes[key];
-    console.log(node.data);
+    console.log("Node: " + node.data);
+    console.log("Neighbors: ");
     for (let i = 0; i < node.adjacentNodes.length; i++) {
       console.log(node.adjacentNodes[i].data);
     }
   }
 
+  //finds the shortest path from a start to end node using BFS algorithm
   findPath(start, end) {
     let queue = [start];
     let visited = new Set();
-    let currNode = start;
     let cameFrom = new Map();
-    visited.add(currNode);
     while (queue.length != 0) {
-      if (currNode === end) {
+      let currNode = queue.shift();
+      if (
+        currNode.data[0] === end.data[0] &&
+        currNode.data[1] === end.data[1]
+      ) {
         let path = [];
         let prev = end;
         while (prev !== start) {
@@ -129,26 +139,31 @@ class Graph {
           cameFrom.set(neighbor, currNode);
         }
       }
-      currNode = queue.shift();
     }
     return null;
   }
 
+  //function to find path from start to end and pront results
   knightMoves(start, end) {
     let startKey = JSON.stringify(start);
     let endKey = JSON.stringify(end);
     let startNode = this.nodes[startKey];
     let endNode = this.nodes[endKey];
     let path = this.findPath(startNode, endNode);
+    if (path === null) {
+      console.log("no path found");
+    }
     console.log(`You made it in ${path.length - 1} moves! Here is your path:`);
     console.log(path.map((node) => node.data));
   }
 }
 
+//initialize and construct graph
 let graph = new Graph();
 
 graph.constructGraph();
 
 graph.linkNodes();
 
+//test case
 graph.knightMoves([0, 0], [7, 7]);
